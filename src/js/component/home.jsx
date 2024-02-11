7b4963125755074c696c87deb8d926e3c578419d
nick <150211508+n1cksway@users.noreply.github.com>
2024-02-11 17:44:44 +0000

    nicksway commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [ todos, setTodos ] = useState([]);  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+			<ul className="container-fluid list-group list-group-flush">
+				<h1>todos</h1>
+					<li className="list-group-item p-0"><input
+						type="text" 
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e)=> {
+							if(e.key === "Enter"){
+								setTodos(todos.concat(inputValue));
+								setInputValue("");
+							}
+
+						}}
+						placeholder="What you need to do"></input>
+					</li>
+					{todos.map((item,index) => (
+					<li className="list-group-item d-inline-flex justify-content-between">
+						{item} <span><i className="fas fa-trash-alt"
+							onClick={() => 
+							setTodos(todos.filter((item,curentIndex) =>
+							index != curentIndex
+							))}></i></span>
+					</li>
+					))}
+				<div id="footer">{todos.length} left to do</div>
+			</ul>
 		</div>
 	);
 };
